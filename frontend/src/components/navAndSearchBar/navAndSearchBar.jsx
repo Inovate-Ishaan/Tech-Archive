47734ab25c681bfc394663441e16d5143d91792f
Krish <ishaanshukla.rn@gmail.com>
2026-06-29 14:49:36 +0530

    togglable side menu created

--- frontend/src/components/navAndSearchBar/navAndSearchBar.jsx
+++ frontend/src/components/navAndSearchBar/navAndSearchBar.jsx
@@ -1,96 +1,151 @@
 //searchBarSmall is a child component of this (NavWithSearch) component
 //searchIcon click calls handleSearch on devices > 600px, but opens searchBar (handleSearchIconClick) on devices < 600px
 
+//sideMenuToggle is also a child of this component
+//rendering the navAndSearchBar will contain the side menu that is toggled using the hamburger icon
 
 import { useEffect, useState } from "react";
 import Button from "../buttons/button";
 import PasswordField from "../passwordField/passwordField";
 import styles from "./navAndSearchBar.module.css";
 import SearchBarSmall from "./searchBarSmall.jsx";
+import SideMenuToggle from "../sideMenu/sideMenuToggle.jsx";
 
-export default function NavWithSearch() {
+export default function NavWithSearch({ className, sideMenuVisible, setSideMenuVisible}) {
+  const [searchQuery, setSearchQuery] = useState("");
 
-    const [searchQuery, setSearchQuery] = useState("");
+  function clearSearchBar() {
+    setSearchQuery("");
+  }
 
-    function clearSearchBar() {
-        setSearchQuery("");
-    };
+  //Searching
+  const [isSearching, setIsSearching] = useState(false);
 
-    //Searching
-    const [isSearching, setIsSearching] = useState(false);
-
-    function handleSearch() {
-        if (!searchQuery) {
-            return
-        }
-        
-        setIsSearching(true);
+  function handleSearch() {
+    if (!searchQuery) {
+      return;
     }
 
+    setIsSearching(true);
+  }
 
-    //changing the searchbar UI for mobile devices
-    const [onSmallDevice, setOnSmallDevice] = useState(window.innerWidth < 600);
-    const [isSearchBarVisible, setIsSearchBarVisible] = useState(false);
+  //changing the searchbar UI for mobile devices
+  const [onSmallDevice, setOnSmallDevice] = useState(window.innerWidth < 600);
+  const [isSearchBarVisible, setIsSearchBarVisible] = useState(false);
 
-    useEffect(() => {
-        function handleResize() {
-            setOnSmallDevice(window.innerWidth < 600);
-        }
+  useEffect(() => {
+    function handleResize() {
+      setOnSmallDevice(window.innerWidth < 600);
+    }
 
-        window.addEventListener("resize", handleResize)
+    window.addEventListener("resize", handleResize);
 
-        return () => {
-            window.removeEventListener("resize", handleResize)
-        }
-    }, []);
-
-    //open the searchbar for smaller devices
-    function handleSearchIconClick() {
-        setIsSearchBarVisible(true);
+    return () => {
+      window.removeEventListener("resize", handleResize);
     };
+  }, []);
 
-    function handleLeftArrowClick () {
-        setIsSearchBarVisible(false);
-    };
+  //open the searchbar for smaller devices
+  function handleSearchIconClick() {
+    setIsSearchBarVisible(true);
+  }
 
-    return (
-        <>
-        {/*searchbar for mobile devices ( < 600px ), visible when search icon is clicked*/}
-        {(isSearchBarVisible && onSmallDevice) && <SearchBarSmall searchQuery={searchQuery} onChange={e => setSearchQuery(e.target.value)} handleSearch={handleSearch} clearSearchBar={clearSearchBar} isSearchBarVisible={isSearchBarVisible} hideSearchBar={handleLeftArrowClick} /> }
-        
-        {/*Parent searchbar component */}
-        {!(isSearchBarVisible && onSmallDevice) && <>
+  function handleLeftArrowClick() {
+    setIsSearchBarVisible(false);
+  }
+
+  //toggling of the side menu (hamburger)
+  //state has been uplifted to feed.jsx to enable darkening of the screen when menu is open
+  const isSideMenuVisible = sideMenuVisible;
+  const setIsSideMenuVisible = setSideMenuVisible;
+
+  const showSideMenu = () => {
+    setSideMenuVisible(true);
+  }
+  const hideSideMenu = () => {
+    setSideMenuVisible(false)
+  }
+
+  return (
+    <>
+      <div className={className}>
         <div className={styles.container}>
-            <div className={styles.left}>
-                <span className={`material-symbols-outlined icon`}>menu</span>
+          {/*searchbar for mobile devices ( < 600px ), visible when search icon is clicked*/}
+          {isSearchBarVisible && onSmallDevice && (
+            <SearchBarSmall
+              searchQuery={searchQuery}
+              onChange={(e) => setSearchQuery(e.target.value)}
+              handleSearch={handleSearch}
+              clearSearchBar={clearSearchBar}
+              isSearchBarVisible={isSearchBarVisible}
+              hideSearchBar={handleLeftArrowClick}
+            />
+          )}
+
+          {/*Parent searchbar component */}
+          {!(isSearchBarVisible && onSmallDevice) && (
+            <>
+              <div className={styles.left}>
+                <span className={`material-symbols-outlined icon`} onClick={showSideMenu}>menu</span>
                 <h2 className="title">Tech Archive</h2>
+              </div>
 
-            </div>
-
-            <div className={styles.center}>
+              <div className={styles.center}>
                 <div className={styles.searchBar}>
-                    <input type="text" placeholder="Search Projects" value={searchQuery} onChange={e => setSearchQuery(e.target.value)}></input>
-                    
-                    {/*Close Button (hide permanently on small screens)*/}
-                    {(searchQuery && !onSmallDevice) && <span onClick={clearSearchBar} className={`material-symbols-outlined icon`} id={styles.closeBtn}>close</span>}
+                  <input
+                    type="text"
+                    placeholder="Search Projects"
+                    value={searchQuery}
+                    onChange={(e) => setSearchQuery(e.target.value)}
+                  ></input>
 
-                    {/*Search Icon*/}
-                    <span className={`material-symbols-outlined icon`} id={styles.searchIcon} onClick={onSmallDevice ? handleSearchIconClick : handleSearch}>search</span>
-                    {isSearching && <p>Searching for {searchQuery}</p>}
+                  {/*Close Button (hide permanently on small screens)*/}
+                  {searchQuery && !onSmallDevice && (
+                    <span
+                      onClick={clearSearchBar}
+                      className={`material-symbols-outlined icon`}
+                      id={styles.closeBtn}
+                    >
+                      close
+                    </span>
+                  )}
 
+                  {/*Search Icon*/}
+                  <span
+                    className={`material-symbols-outlined icon`}
+                    id={styles.searchIcon}
+                    onClick={
+                      onSmallDevice ? handleSearchIconClick : handleSearch
+                    }
+                  >
+                    search
+                  </span>
+                  {isSearching && <p>Searching for {searchQuery}</p>}
                 </div>
                 <span className={`material-symbols-outlined icon`}>sort</span>
-            </div>
+              </div>
 
-            <div className={styles.right}>
-            <Button variant="secondaryBlack" status="active">
-                <span className={`material-symbols-outlined icon`}>add_2</span>
-                Post
-            </Button>
-            <span className={`material-symbols-outlined icon`}>account_circle</span>
-            </div>
+              <div className={styles.right}>
+                <Button variant="secondaryBlack" status="active">
+                  <span className={`material-symbols-outlined icon`}>
+                    add_2
+                  </span>
+                  Post
+                </Button>
+                <span className={`material-symbols-outlined icon`}>
+                  account_circle
+                </span>
+              </div>
+            </>
+          )}
         </div>
-        </>}
-        </>
-    )
-};
+
+        {sideMenuVisible && <SideMenuToggle
+          className={styles.sideMenuToggle}
+          selectedOption={"home"}
+          closeBtnFunction={hideSideMenu}
+        /> }
+      </div>
+    </>
+  );
+}
--- frontend/src/pages/feed.jsx
+++ frontend/src/pages/feed.jsx
@@ -1,17 +1,33 @@
+import { useEffect, useState } from "react";
 import Footer from "../components/footer/footer";
 import NavWithSearch from "../components/navAndSearchBar/navAndSearchBar";
 import PostCard from "../components/postCard/postCard";
 import SideMenuFixed from "../components/sideMenu/sideMenuFixed";
+import SideMenuToggle from "../components/sideMenu/sideMenuToggle";
 import styles from "./feed.module.css";
 
 export default function FeedPage() {
+
+  //to darken the remaining page when the side menu toggle is open
+  const [sideMenuToggleVisible, setSideMenuToggleVisible] = useState(false);
+  //prevent scroll when the side menu is open
+
+  useEffect( () => {
+    document.body.style.overflow = sideMenuToggleVisible ? "hidden" : "auto";
+
+    return () => {
+      document.body.style.overflow = 'auto';
+    };
+  }, [sideMenuToggleVisible]);
+
   return (
     <>
-      <NavWithSearch />
+      <NavWithSearch className={styles.navWithSearch} sideMenuVisible={sideMenuToggleVisible} setSideMenuVisible={setSideMenuToggleVisible}/>
 
       {/*Feed Body*/}
-      <div className={styles.feedBody}>
+      <div className={`styles.feedBody ${sideMenuToggleVisible ? styles.darken : ""}`}>
 
+      
       <SideMenuFixed className={styles.sideMenuFixed} selectedOption={"home"} />
 
       <div className={styles.feed}>
